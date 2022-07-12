cca755ced6609651b854b93fab10315d27627cea
cemayan <cem_ayan@outlook.com>
2022-07-13 01:19:15 +0300

    application.yml's changed.

--- music-service/music-cmd-api/src/main/java/com/ca/musiccmdapi/MusicCommandApplication.java
+++ music-service/music-cmd-api/src/main/java/com/ca/musiccmdapi/MusicCommandApplication.java
@@ -3,8 +3,10 @@ package com.ca.musiccmdapi;
 import com.ca.musiccore.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
@@ -13,6 +15,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Import({AxonConfig.class})
 @EnableDiscoveryClient
+@EnableR2dbcRepositories
+@EntityScan(basePackages = "com.ca.musiccore.models")
 public class MusicCommandApplication {
 
     public static void main(String[] args) {
